25a8cf9cdc72faadfa7dc7b3f9c495c84d55bb90
wzk <1403235458@qq.com>
2021-11-12 21:19:05 +0800

    初次创建后端框架

---
+++ src/main/java/com/BlogSystem.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 运行博客系统的启动类
+ */
+@SpringBootApplication
+public class BlogSystem {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSystem.class,args);
+    }
+}
